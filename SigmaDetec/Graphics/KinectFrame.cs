5b5b3c6c87f1136d16ac242c987b7b035e496491
senbar <barsen97@gmail.com>
2018-04-25 09:06:20 -0700

    refractor

--- SigmaDetec/Graphics/KinectFrame.cs
+++ SigmaDetec/Graphics/KinectFrame.cs
@@ -4,9 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
 namespace SigmaDetec.Graphics
-{
-    class KinectFrame
     {
+        class KinectFrame
+        {
+            public Bitmap colorDrawingBitmap;
+            private Int32Rect cropRect;
+
+            public KinectFrame()
+            {
+                cropRect = Int32Rect.Empty;
+                colorDrawingBitmap = new Bitmap(640, 480, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            }
+
+            public void LoadBitmap(ColorImageFrame Image, byte[] pixeldata)
+            {
+
+                BitmapData bmapdata = colorDrawingBitmap.LockBits(
+                    new Rectangle(0, 0, Image.Width, Image.Height),
+                    ImageLockMode.WriteOnly,
+                    colorDrawingBitmap.PixelFormat);
+                IntPtr ptr = bmapdata.Scan0;
+                Marshal.Copy(pixeldata, 0, ptr, Image.PixelDataLength);
+                colorDrawingBitmap.UnlockBits(bmapdata);
+            }
+
+            /// <summary>
+            /// return image source to use in WPF
+            /// </summary>
+            /// <returns></returns>
+            public ImageSource GetImageSource()
+            {
+                return ImageSourceForBitmap(this.colorDrawingBitmap);
+            }
+
+            //If you get 'dllimport unknown'-, then add 'using System.Runtime.InteropServices;'
+            [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool DeleteObject([In] IntPtr hObject);
+
+            private ImageSource ImageSourceForBitmap(Bitmap bmp)
+            {
+                var handle = bmp.GetHbitmap();
+                try
+                {
+                    return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, cropRect, BitmapSizeOptions.FromEmptyOptions());
+                }
+                finally { DeleteObject(handle); }
+            }
+        }
     }
+
+}
 }
--- SigmaDetec/Graphics/TrackingFrame.cs
+++ SigmaDetec/Graphics/TrackingFrame.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SigmaDetec.Graphics
 {
-    class TrackingFrame
+    static class Tracking 
     {
+        /// <summary>
+        /// draw rectangle with given coordinates to highlist detected object
+        /// </summary>
+        /// <param name="Rect"></param>
+        static public void DrawRectangle(Rectangle Rect)
+        {
+            using (System.Drawing.Graphics bmpGraphics =
+                System.Drawing.Graphics.FromImage(this.colorDrawingBitmap))
+            {
+                bmpGraphics.DrawRectangle(new System.Drawing.Pen(System.Drawing.Brushes.Pink, 5), Rect);
+
+            }
+        }
     }
 }
