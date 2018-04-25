c1debbd13c1f31f3b97038f65407f2c733417a30
MacenDi <mstanekofficial@gmail.com>
2018-04-25 17:34:54 +0200

    Creating rectangle works

---
+++ SigmaDetec/RectangleCreator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaDetec
+{
+    public class RectangleCreator
+    {
+        public List<SigamDetec.Point> ListOfPoints;
+        public RectangleCreator()
+        {
+            ListOfPoints = new List<SigamDetec.Point>();
+        }
+        public void AddPoint(SigamDetec.Point pointToAdd)
+        {
+            ListOfPoints.Add(pointToAdd);
+        }
+        public Rectangle CreateRectangle()
+        {
+            if (ListOfPoints.Count == 2)
+            {
+                var height = ListOfPoints[0].X - ListOfPoints[1].X;
+                var width = ListOfPoints[0].Y - ListOfPoints[1].Y;
+
+                var averageDistance = Math.Abs((height + width) / 2);
+                
+                return new Rectangle((int)ListOfPoints[0].X, (int)ListOfPoints[0].Y, (int)averageDistance, (int)averageDistance);
+            }
+            else
+            {
+                return new Rectangle(0, 0, 0, 0);
+
+            }
+        }
+    }
+}
